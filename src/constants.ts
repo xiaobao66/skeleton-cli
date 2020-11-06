4c37c1ef0ba2bf5764bd9a40500cec36ca771411
xiaobaowei <xiaobao66@bingyan.net>
2020-11-06 19:22:01 +0800

    feat: 新增vanillajs模板

--- src/constants.ts
+++ src/constants.ts
@@ -6,11 +6,13 @@ const TEMPLATE_TYPES: {
   [key: string]: string;
   REACT: string;
   REACT_TS: string;
+  VANILLAJS: string;
   VANILLAJS_TS: string;
   TOOL_TS: string;
 } = {
   REACT: 'REACT',
   REACT_TS: 'REACT_TS',
+  VANILLAJS: 'VANILLAJS',
   VANILLAJS_TS: 'VANILLAJS_TS',
   TOOL_TS: 'TOOL_TS',
 };
@@ -19,6 +21,7 @@ const TEMPLATE_URLS = {
   [TEMPLATE_TYPES.REACT]: 'https://github.com/xiaobao66/react-skeleton.git',
   [TEMPLATE_TYPES.REACT_TS]:
     'https://github.com/xiaobao66/react-skeleton-ts.git',
+  [TEMPLATE_TYPES.VANILLAJS]: 'https://github.com/xiaobao66/vanillajs-skeleton.git',
   [TEMPLATE_TYPES.VANILLAJS_TS]:
     'https://github.com/xiaobao66/vanillajs-skeleton-ts.git',
   [TEMPLATE_TYPES.TOOL_TS]: 'https://github.com/xiaobao66/tool-skeleton-ts.git',
